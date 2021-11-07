433402c5bf9f96ef79daf75dedb563bb5863c7c7
jahkyxd <82198115+jahkyxd@users.noreply.github.com>
2021-11-07 15:17:21 +0300

    Add files via upload

---
+++ ceki.js
@@ -0,0 +1,53 @@
+const { Client } = require("discord.js");
+const request = require("request")
+
+const tokens = ["token 1", "token 2", "token 3", "böyle devam et"];
+const options = {
+    url: "",
+    GuildID: "",
+    log: ""
+}
+
+let durum = false
+
+tokens.forEach(token => {
+    const client = new Client({
+        fetchAllMembers: true,
+        presence: {
+            activity: { name: "Developed By Jahky.", type: "LISTENING" }, status: "dnd"
+        }
+    });
+
+    client.on("ready", () => {
+        if (durum) { URL(); }
+        console.log(`${client.guilds.cache.get(options.GuildID).name} adlı sunucuda ${options.url} urlsi spamlanmaya başlandı`)
+        setInterval(() => {
+            if (durum) { URL(); }
+            let sunucu = client.guilds.cache.get(options.GuildID);
+            if (sunucu.vanityURLCode === options.url) {
+                client.channels.cache.get(options.log).send(`@everyone ${sunucu.name} adlı sunucunun url si çalınmıştır!`)
+                console.log("URL çalındı bottan çıkış yapılıyor ...")
+                process.exit(0)
+            }
+        }, 1);
+    })
+
+    function URL() {
+        let sunucu = client.guilds.cache.get(options.GuildID);
+        if (!sunucu.vanityURLCode || sunucu.vanityURLCode === options.url)
+            if (sunucu.vanityURLCode !== options.url) {
+                request({
+                    method: "PATCH",
+                    url: `https://discord.com/api/guilds/${options.GuildID}/vanity-url`,
+                    headers: {
+                        "Authorization": `Bot ${client.token}`
+                    },
+                    json: {
+                        "code": `${options.url}`
+                    }
+                });
+            }
+    }
+
+    client.login(token).then(x => console.log(`${client.user.tag}`))
+})
---
+++ readme.md
@@ -0,0 +1,12 @@
+# ÖNEMLİ BİLGİLER
+bu url spammer diğerlerinden farklıdır tek bot üzerinden değilde bir çok botun üzerinden spamlar böylece url yi ele geçirmeniz daha kolay olur main dosyasındaki options u doldurun çalışır.
+
+# OKUMADAN GEÇME
+glitch kullananlar glitche uygun `package.json` dosyası koymalıdırlar
+
+# NOT
+altyapıyı hiç denemedim ama url fonksiyonunu kendi guard botumda ne olur ne olmaz diye spamlatıyordum ordan aldım haberiniz olsun çalışması lazım yani xrd
+
+[Youtube Matthe](https://www.youtube.com/c/Matthecim)
+[Github Jahky](https://github.com/jahkyxd/)
+[Github Matthe](https://github.com/Matthejs)
